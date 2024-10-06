79e38435d8218888b57a8706edbebb43dce0da20
Baimenovbn <baibn@mail.ru>
2024-10-06 17:21:13 +0500

    feat: finish cli:args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,17 @@
 const parseArgs = () => {
-    // Write your code here 
+  const result = [];
+
+
+  const args = process.argv.slice(2);
+  args.forEach((arg, i) => {
+    if (arg.startsWith('--')) {
+      const probableVal = args[i + 1].startsWith('--') ? '' : args[i + 1];
+
+      result.push(`${arg} is ${probableVal}`);
+    }
+  });
+
+  console.log(result.join(', '))
 };
 
 parseArgs();
