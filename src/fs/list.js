7f2e10de53ebc0e9b0006e7adbe431f5e39f4154
Baimenovbn <baibn@mail.ru>
2024-10-06 17:03:16 +0500

    feat: finish cp:list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,16 @@
+import { readdir } from 'fs/promises';
+import path from 'path';
+import { FsOperationFailedError } from '../utils/index.js';
+
+const dest = path.join(import.meta.dirname, './files');
+
 const list = async () => {
-    // Write your code here 
+  try {
+    const files = await readdir(dest);
+    files.forEach(n => console.log(n));
+  } catch {
+    throw new FsOperationFailedError();
+  }
 };
 
 await list();
