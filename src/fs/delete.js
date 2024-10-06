1593f7c1c90807072ee24b274f6a98c9f1c8e32b
Baimenovbn <baibn@mail.ru>
2024-10-06 17:00:20 +0500

    feat: finish cp:delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,15 @@
+import { rm } from 'node:fs/promises';
+import path from 'path';
+import { FsOperationFailedError } from '../utils/index.js';
+
+const fileToRemove = path.join(import.meta.dirname, './files/fileToRemove.txt');
+
 const remove = async () => {
-    // Write your code here 
+  try {
+    await rm(fileToRemove);
+  } catch {
+    throw new FsOperationFailedError();
+  }
 };
 
 await remove();
