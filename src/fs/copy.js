d2bbd00a315b8b16564c3670a163c0f05f352794
Baimenovbn <baibn@mail.ru>
2024-10-06 16:28:48 +0500

    feat: finish cp:copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,17 @@
+import { cp } from 'node:fs/promises';
+import path from 'path';
+import { FsOperationFailedError } from '../utils/index.js';
+
+const src = path.join(import.meta.dirname, './files');
+const dest = path.join(import.meta.dirname, './files_copy');
+
 const copy = async () => {
-    // Write your code here 
+  try {
+    await cp(src, dest, { errorOnExist: true, recursive: true, force: false });
+  } catch (e) {
+    throw new FsOperationFailedError();
+  }
+
 };
 
 await copy();
