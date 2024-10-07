3a5428f3d53166174ec55fff831f6d9745bd8191
Baimenovbn <baibn@mail.ru>
2024-10-07 18:00:03 +0500

    feat: finish zip

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,15 @@
+import path from 'path';
+import { createReadStream, createWriteStream } from 'fs';
+import zlib from 'zlib';
+
+const src = path.join(import.meta.dirname, './files/archive.gz');
+const dest = path.join(import.meta.dirname, './files/fileToCompress.txt');
+
 const decompress = async () => {
-    // Write your code here 
+  const input = createReadStream(src);
+  const gzip = zlib.createGunzip();
+
+  input.pipe(gzip).pipe(createWriteStream(dest));
 };
 
 await decompress();
