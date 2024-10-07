6ffb5f47fd0af85ab9cda811624c96ac7e6d0b5c
Baimenovbn <baibn@mail.ru>
2024-10-07 19:26:23 +0500

    feat: finish worker threads

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,27 @@
+import { Worker } from 'worker_threads';
+import os from 'os';
+import path from 'path';
+
+const pathToWorker = path.join(import.meta.dirname, './worker.js');
+
 const performCalculations = async () => {
-    // Write your code here
+  const numOfCPUs = os.cpus().length;
+  const startNum = 10;
+  const result = [];
+
+  for (let i = 0; i < numOfCPUs; i++) {
+    const worker = new Worker(pathToWorker, { workerData: i + startNum });
+    result[i] = new Promise((res) => {
+      worker.on('message', (data) => {
+        res({ data, status: 'resolved' });
+      });
+      worker.on('error', () => {
+        res({ data: null, status: 'rejected' });
+      });
+    })
+  }
+
+  console.log(await Promise.all(result));
 };
 
 await performCalculations();
