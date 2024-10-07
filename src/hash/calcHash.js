58716dc651a2978fc81f4af17e646dc47d216491
Baimenovbn <baibn@mail.ru>
2024-10-07 17:47:37 +0500

    feat: finish hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,11 @@
+import { createReadStream } from 'fs';
+import { createHash } from 'crypto';
+import path from 'path';
+
+const src = path.join(import.meta.dirname, './files/fileToCalculateHashFor.txt');
+
 const calculateHash = async () => {
-    // Write your code here 
+  createReadStream(src).pipe(createHash('sha256').setEncoding('hex')).pipe(process.stdout);
 };
 
 await calculateHash();
